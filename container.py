1001759e17de3aceddae1c7b45ced7aa53f8440e
jhon_carmona <ac563466@gmail.com>
2025-04-23 23:48:25 -0500

    update documentation

--- container.py
+++ container.py
@@ -2,13 +2,14 @@ from tkinter import *
 import tkinter as tk
 from ventas import Ventas
 from inventario import Inventario
+from PIL import Image, ImageTk
 
 
 class Container(tk.Frame):
     def __init__(self, padre, controlador):
         super().__init__(padre)
         self.controlador = controlador
-        self.pack()
+        # self.pack() # No es necesario si usas place en el mismo widget
         self.place(x=0, y=0, width=800, height=400)
         self.config(bg="#C6D9E3")
         self.widgets()
@@ -30,13 +31,23 @@ class Container(tk.Frame):
     def widgets(self):
 
         frame1 = tk.Frame(self, bg="#C6D9E3")
-        frame1.pack()
         frame1.place(x=0, y=0, width=800, height=400)
 
-        btnventas = Button(frame1, bg="green", fg="black",
-                        text="Ir a ventas", command=self.ventas)
+        btnventas = Button(frame1, bg="#f4b400", fg="white",
+                           font=("sans", 18, "bold"), text="Ir a ventas", command=self.ventas)
         btnventas.place(x=500, y=30, width=240, height=60)
 
-        btninventario = Button(frame1, bg="blue", fg="white",
-                            text="Ir a inventario", command=self.inventario)
+        btninventario = Button(frame1, bg="#c62e26", fg="white",
+                               font=("sans", 18, "bold"), text="Ir a inventario", command=self.inventario)
         btninventario.place(x=500, y=130, width=240, height=60)
+
+        self.logo_image = Image.open("imagenes/registradora.png")
+        self.logo_image = self.logo_image.resize((280, 280))
+        self.logo_image = ImageTk.PhotoImage(self.logo_image)
+        self.logo_label = tk.Label(
+            frame1, image=self.logo_image, bg="#C6D9E3")
+        self.logo_label.place(x=100, y=30)
+
+        copyright_label = tk.Label(
+            frame1, text=" 2025 uniremington code. Todos los derechos reservados", font="sans 12 bold", bg="#C6D9E3", fg="gray")
+        copyright_label.place(x=180, y=350)
--- ventas.py
+++ ventas.py
@@ -6,14 +6,97 @@ from tkinter import ttk, messagebox
 class Ventas(tk.Frame):
 
     def __init__(self, parent):
-        super().__init__(parent)
+        super().__init__(parent)  # 1100, 650
         self.widgets()  # Llama al método widgets aquí
 
     def widgets(self):
 
         frame1 = tk.Frame(self, bg="#dddddd")
+        # Corrección: Usar solo place
         frame1.place(x=0, y=0, width=1100, height=100)
 
         titulo = tk.Label(self, text="VENTAS", bg="#dddddd",
-                          font="sans 30 bold", anchor="center")
+                          font=("sans", 30, "bold"), anchor="center")  # Corrección: Sintaxis de font y usar solo place
         titulo.place(x=5, y=0, width=1090, height=90)
+
+        frame2 = tk.Frame(self, bg="#C6D9E3")
+        frame2.place(x=0, y=100, width=1100, height=550)
+
+        lblframe = LabelFrame(
+            frame2, text="Infromacion de la venta", bg="#C6D9E3", font="sans 16 bold")
+        lblframe.place(x=10, y=10, width=1060, height=80)
+
+        label_numero_factura = tk.Label(
+            lblframe, text="Numero de \nfactura", bg="#C6D9E3", font="sans 12 bold")
+        label_numero_factura.place(x=10, y=5)
+        self.numero_factura = tk.StringVar()
+
+        self.entry_numero_factura = ttk.Entry(
+            lblframe, textvariable=self.numero_factura, state="readonly", font="sans 12 bold")
+        self.entry_numero_factura.place(x=100, y=5, width=80)
+
+        label_nombre = tk.Label(
+            lblframe, text="Producto: ", bg="#C6D9E3", font="sans 12 bold")
+        label_nombre.place(x=200, y=12)
+
+        self.entry_nombre = ttk.Entry(lblframe, font="sans 12 bold")
+        self.entry_nombre.place(x=280, y=10, width=180)
+
+        label_valor = tk.Label(lblframe, text="precio: ",
+                               bg="#C6D9E3", font="sans 12 bold")
+        label_valor.place(x=470, y=12)
+
+        self.entry_valor = ttk.Entry(lblframe, font="sans 12 bold")
+        self.entry_valor.place(x=540, y=10, width=180)
+
+        label_cantidad = tk.Label(
+            lblframe, text="Cantidad: ", bg="#C6D9E3", font="sans 12 bold")
+        label_cantidad.place(x=730, y=12)
+
+        self.entry_cantidad = ttk.Entry(lblframe, font="sans 12 bold")
+        self.entry_cantidad.place(x=820, y=10)
+
+        treFrame = tk.Frame(frame2, bg="#C6D9E3")
+        treFrame.place(x=150, y=120, width=800, height=200)
+
+        scrol_y = ttk.Scrollbar(treFrame, orient=VERTICAL)
+        scrol_y.pack(side=RIGHT, fill=Y)  # Corrección: Usar la constante Y
+
+        scrol_x = ttk.Scrollbar(treFrame, orient=HORIZONTAL)
+        scrol_x.pack(side=BOTTOM, fill=X)  # Corrección: Usar la constante X
+
+        self.tree = ttk.Treeview(treFrame, columns=(
+            "Producto", "Precio", "Cantidad", "Subtotal"), show="headings", height=10,
+            yscrollcommand=scrol_y.set, xscrollcommand=scrol_x.set)  # Corrección: Usar '='
+
+        scrol_y.config(command=self.tree.yview)
+        # Corrección: Usar .xview para la barra horizontal
+        scrol_x.config(command=self.tree.xview)
+
+        self.tree.heading("#1", text="Producto")
+        self.tree.heading("#2", text="Precio")
+        self.tree.heading("#3", text="Cantidad")
+        self.tree.heading("#4", text="Subtotal")
+
+        self.tree.column("#1", anchor="center")
+        self.tree.column("#2", anchor="center")
+        self.tree.column("#3", anchor="center")
+        self.tree.column("#4", anchor="center")
+
+        self.tree.pack(expand=True, fill=BOTH)
+
+        lblframe1 = LabelFrame(frame2, text="Opciones",
+                               bg="#C6D9E3", font="sans 12 bold")
+        lblframe1.place(x=10, y=300, width=1060, height=100)
+
+        boton_agregar = tk.Button(
+            lblframe1, text="Agregar Articulo", bg="#dddddd", font="sans 12 bold")
+        boton_agregar.place(x=50, y=10, width=240, height=50)
+
+        boton_pagar = tk.Button(
+            lblframe1, text="Pagar", bg="#dddddd", font="sans 12 bold")
+        boton_pagar.place(x=400, y=10, width=240, height=50)
+
+        boton_ver_facturas = tk.Button(
+            lblframe1, text="Ver facturas", bg="#dddddd", font="sans 12 bold")
+        boton_ver_facturas.place(x=750, y=10, width=240, height=50)
